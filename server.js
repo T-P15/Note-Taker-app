7aaea5ad455d874af6dd2aa9affc70d22402b521
mingosis <tom.piccirillo15@gmail.com>
2023-11-08 20:10:36 +1000

    * path

--- server.js
+++ server.js
@@ -19,14 +19,10 @@ app.get('/routes', (req, res) =>
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
